8ee12dfd2c5486607d6e9363b3f43647ffdac633
KarlSemilla <karlsemilla1@gmail.com>
2021-03-07 23:27:40 -0700

    Changed the guild tag from [TCE] to [AnE]

--- commands/giveaway.js
+++ commands/giveaway.js
@@ -5,7 +5,7 @@ const giveawaySchema = require('../schemas/giveaway-schema');
 const giveawayTimer = require('../handlers/giveaway-timer');
 
 const giveawayMessage =`
-**[TCE] Weekly Guild Giveaway signups are now open!**
+**[AnE] Weekly Guild Giveaway signups are now open!**
 
 Enrollment is open from now until 1 hour after reset on Sunday night (Monday morning for those on the east half of the world). The possible rewards can be seen in our guild's Deep Cave (last tab in the guild bank) and range anywhere from 1 to 80 gold in value*. The rules are simple:
 1) Guild Members only (no officers or leaders can enter)
